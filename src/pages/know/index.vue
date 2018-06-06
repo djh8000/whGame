954cd5af696d83233f62572ceffb4b6a3c82796e
lm <kissminmin1207@qq.com>
2018-06-06 22:46:00 +0800

    sd

---
+++ src/pages/know/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="knowIndex">
+    <div class="mainTit"></div>
+    <div class="knowRole">
+      <img src="../../assets/img/m-icon5.png" class="roleTit">
+      <div class="p">
+        <p>游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则介绍内容文字游戏规则游戏规则</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss" scoped>
+  .knowIndex{
+    height: 100vh;
+    background: url('../../assets/img/m-bg2.png') center bottom no-repeat;
+    overflow: hidden;
+    position: relative;
+    .mainTit{
+      width: 428px;
+      height: 291px;
+      margin-top:50px;
+      margin-left: 186px;
+      
+    }
+    .knowRole{
+      width: 620px;
+      height: 450px;
+      margin:65px auto 0;
+      background-color: #fff;
+      border-radius: 25px;
+      padding: 80px 65px;
+      position: relative;
+      .roleTit{
+        position: absolute;
+        left: 50%;
+        margin: -130px;
+        top: 95px;
+      }
+      .p{
+        overflow: auto;
+        width: 100%;height: 100%;
+      }
+      p{
+        color: #5f4f4b;
+        font-size: 28px;
+        line-height: 48px;
+        text-align: justify;
+      }
+    }
+  }
+</style>
+
